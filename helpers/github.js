cff20919e58276e68519b25160b8c5157b5967b0
inh556 <yanbin.jin@gmail.com>
2018-04-07 15:58:54 -0700

    make fetch data from github success

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,29 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (user) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
 
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${user}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
-
+  function callback(error, response, body) {
+    console.log("github is working")
+    if (!error && response.statusCode === 200) {
+      var info = JSON.parse(body);
+      console.log(info[0].id);
+      console.log(info[0].name);
+      console.log(info[0].html_url);
+    }
+  };
+  request(options, callback);
 }
 
 module.exports.getReposByUsername = getReposByUsername;
